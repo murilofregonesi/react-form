0935fbe5f2fdfff6409cd3231f753439658c8d48
Murilo Fregonesi Falleiros <murilo.fregonesi@sensedata.com.br>
2022-04-11 19:49:30 -0300

    First commit

---
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function RegistrationForm() {
+    return(
+        <form>
+            <input type="text" />
+        </form>
+    );
+}
+
+export default RegistrationForm;
