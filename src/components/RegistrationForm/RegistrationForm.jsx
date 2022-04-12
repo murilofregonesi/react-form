0be24b814de927825538a88074de21c80c7288de
Murilo Fregonesi Falleiros <murilo.fregonesi@sensedata.com.br>
2022-04-12 08:19:50 -0300

    feat: MaterialUI

--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -1,10 +1,26 @@
 import React from "react";
+import Button from '@mui/material/Button';
 
 
 function RegistrationForm() {
     return(
         <form>
+            <label>Nome</label>
             <input type="text" />
+            
+            <label>Sobrenome</label>
+            <input type="text" />
+            
+            <label>CPF</label>
+            <input type="text" />
+            
+            <label>Newsletter</label>
+            <input type="checkbox" />
+            
+            <label>Promoções</label>
+            <input type="checkbox" />
+            
+            <Button variant="contained">Cadastrar</Button>
         </form>
     );
 }
