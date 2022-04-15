abf8b5192184a497e0e7e506aecf29e14b83e52d
Murilo Fregonesi Falleiros <murilo.fregonesi@sensedata.com.br>
2022-04-15 11:12:16 -0300

    feat: hooks

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import { Fragment } from 'react';
-import './App.css';
+import React from 'react';
 import RegistrationForm from "./components/RegistrationForm"
 import { Container, Typography } from '@mui/material';
+import './App.css';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
--- src/components/RegistrationForm/RegistrationForm.jsx
+++ src/components/RegistrationForm/RegistrationForm.jsx
@@ -1,20 +1,51 @@
-import React from "react";
-import { TextField, Button, FormControlLabel, Switch } from '@mui/material';
-
+import React, { useState } from "react";
+import { TextField, Button, FormControlLabel, Switch } from "@mui/material";
 
 function RegistrationForm() {
-    return(
-        <form>
-            <TextField id="name" label="Nome" margin="dense" fullWidth />
-            <TextField id="surname" label="Sobrenome" margin="dense" fullWidth />
-            <TextField id="cpf" label="CPF" margin="dense" fullWidth />
-            
-            <FormControlLabel label="Novidades" control={<Switch defaultChecked />} />
-            <FormControlLabel label="Promoções" control={<Switch defaultChecked />} />
+  const [name, setName] = useState("");
+  const [surname, setSurname] = useState("");
+  const [cpf, setCpf] = useState("");
 
-            <Button variant="contained">Cadastrar</Button>
-        </form>
-    );
+  return (
+    <form
+      onSubmit={(e) => {
+        e.preventDefault();
+        console.log(e);
+      }}
+    >
+      <TextField
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        id="name"
+        label="Nome"
+        margin="dense"
+        fullWidth
+      />
+      <TextField
+        value={surname}
+        onChange={(e) => setSurname(e.target.value)}
+        id="surname"
+        label="Sobrenome"
+        margin="dense"
+        fullWidth
+      />
+      <TextField
+        value={cpf}
+        onChange={(e) => setCpf(e.target.value)}
+        id="cpf"
+        label="CPF"
+        margin="dense"
+        fullWidth
+      />
+
+      <FormControlLabel label="Novidades" control={<Switch defaultChecked />} />
+      <FormControlLabel label="Promoções" control={<Switch defaultChecked />} />
+
+      <Button type="submit" variant="contained">
+        Cadastrar
+      </Button>
+    </form>
+  );
 }
 
 export default RegistrationForm;
